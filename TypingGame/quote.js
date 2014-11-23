2768a18804eb44efb82b0accb8d22bcb4810c2c1
Abbas Ally <abbasally5@yahoo.com>
2014-11-23 15:20:20 -0600

    updated typing game

--- TypingGame/quote.js
+++ TypingGame/quote.js
@@ -17,7 +17,34 @@ app.controller('typeCtrl', ['$scope',
 		"Perfection is not attainable, but if we chase perfection " + 
 		"we can catch excellence",
 		"Someone is sitting in the shade today because "+ 
-		"someone planted a tree a long time ago"
+		"someone planted a tree a long time ago",
+		"Abbas likes to type on his mechanical keyboard",
+		"The pumpkin pie at kinsolving is not good",
+		"Vinay likes to hookup with Shannon",
+		"Daniel likes to wear vineyard vines sweaters and shirts",
+		"Abbas wants to learn how to code in C++",
+		"Repeating things over and over and over and over is really annoying",
+		"I like typing!",
+		"The rabbit likes to jump over the cabbages and head straight fo the carrot patch",
+		"Putting chocolate sauce on top of ice cream is good",
+		"George Farcasiu was not the team captain of the Team 3947",
+		"Vinay wants a threesome with Leslie and Jackie",
+		"Deepak's GTI is really fast",
+		"I go to UT, the best University in the world!",
+		"My favorite food is spaghetti",
+		"My new best friend is League of Legends streams",
+		"I like riding shotgun",
+		"I like eating tortillas with cheese and guacamole",
+		"The real problem with America is its availability of cheese",
+		"I am glad I didn't go to UTD",
+		"Jeffrey Smythe is our lord and savior",
+		"Jeffrey is the reason for the season",
+		"Jeffreys elbows move side to side",
+		"Soda is bad for your health",
+		"Everytime a chimpanzee eats a yogurt a cow dies",
+		"My favorite character in Legend of Korra is Bolin",
+		"The best superhero is Iron Man",
+		"Audi R8's are the coolest cars ever made"
 		];
 
 		$scope.randomQuote = function() {
@@ -47,7 +74,7 @@ app.controller('typeCtrl', ['$scope',
 			//console.log("quote " + quote);
 			var htmlString = "";
 			if (!$scope.typed || $scope.typed === null) {
-				htmlString = "<p>" + quote + "</p>";
+				htmlString = "<span>" + quote + "</span>";
 			}
 			else {
 				var correct = stringCorrect($scope.typed, quote);
@@ -75,11 +102,11 @@ app.controller('typeCtrl', ['$scope',
 
 function stringCorrect(typed, text) {
 	var correct = "";
-	var letTyped = typed.split("");
-	var textArr = text.split("");
-	for(i = 0; i < letTyped.length; i++) {
-		if (letTyped[i] == textArr[i]) {
-			correct += letTyped[i];
+	//var letTyped = typed.split("");
+	//var textArr = text.split("");
+	for(i = 0; i < typed.length; i++) {
+		if (typed.charAt(i) == text.charAt(i)) {
+			correct += typed.charAt(i) + "";
 		}
 		else return correct;
 	}
